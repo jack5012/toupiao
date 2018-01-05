63e35485695b07f4d6a22e436ee6db77e5a0372a
zhaoliudong <675352316@qq.com>
2018-01-05 16:55:44 +0800

    修改

--- app/Observers/VoteItemObserver.php
+++ app/Observers/VoteItemObserver.php
@@ -24,9 +24,9 @@ class VoteItemObserver
 
     public function deleted(VoteItem $voteItem)
     {
-        $voteItem->voteProject->involved -= 1;  //从投票主题参与者数量减1
+        /*$voteItem->voteProject->involved -= 1;  //从投票主题参与者数量减1
         $voteItem->voteProject->voted -= $voteItem->voted;  //从投票主题中删除该参与者投票总数
-        $voteItem->voteProject->save();
+        $voteItem->voteProject->save();*/
 
         $voteItem->voteRecord()->delete();   //删除所有该参与者的投票记录
     }
